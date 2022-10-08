ed5677d436f47ec18812bff0cdb6c9ddeecafe47
Paul McKenna <mckennapaul27@gmail.com>
2022-10-08 10:28:09 +0100

    first commit

--- middleware.js
+++ middleware.js
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
 
-export default function middleware(request) {
-    console.log(request);
-    if (request.nextUrl.pathname.startsWith('/about')) {
-        return NextResponse.rewrite(new URL('/about-2', request.url));
-    }
+export function middleware(req) {
+    console.log('Middleware file', req.nextUrl);
+    // if (request.nextUrl.pathname.startsWith('/about')) {
+    //     return NextResponse.rewrite(new URL('/about-2', request.url));
+    // }
 
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
-        return NextResponse.rewrite(new URL('/dashboard/user', request.url));
-    }
+    // if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    //     return NextResponse.rewrite(new URL('/dashboard/user', request.url));
+    // }
+    return NextResponse.rewrite(req.nextUrl);
 }
